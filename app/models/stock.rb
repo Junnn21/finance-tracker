975e7d405312719b1b17fc1d1a84bdd4c026ad59
EC2 Default User <cjun580@gmail.com>
2022-09-19 04:10:12 +0000

    Sign up

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,9 @@
 class Stock < ApplicationRecord
+    has_many :user_stocks
+    has_many :users, through: :user_stocks
+    
+    validates :name, :ticker, presence: true
+    
     def self.new_lookup(ticker_symbol)
         api_key = Rails.application.credentials.fmp[:api_key]
         uriString = "https://financialmodelingprep.com/api/v3/quote/#{ticker_symbol}?apikey=#{api_key}" 
@@ -10,5 +15,9 @@ class Stock < ApplicationRecord
         rescue => exception
             return nil 
         end
+    end
+    
+    def self.check_db(ticker_symbol)
+       where(ticker: ticker_symbol).first 
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :user_stocks, only: [:create, :destroy]
   devise_for :users
   root to: "home#index"
   get 'my_portofolio', to: 'users#my_portofolio'
